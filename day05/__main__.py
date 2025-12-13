7db8f2f6ded3cbb42cec8612896212a9cc028bb6
martin.zeleny <martin.zeleny@betsys.com>
2025-12-13 01:11:32 +0100

    D5 - 2

--- day05/__main__.py
+++ day05/__main__.py
@@ -35,15 +35,29 @@ def b():
 
         fresh.sort()
         print(fresh)
-        last_e = fresh[0][1]
-        result = fresh[0][1] - fresh[0][0] + 1
-        for (s, e) in fresh[1:]:
-            print(last_e, s,e)
-            if s < last_e:
-                s = last_e
+        last_e = 0
+        result = 0
+        # last_e = fresh[0][1]
+        # result = fresh[0][1] - fresh[0][0] + 1
+        for (s, e) in fresh:
+            print("last_e = {}, s = {}, e = {}".format(last_e, s, e))
+
+            if s > last_e:
+                s_calc = s
+                e_calc = e
+            else:
+                s_calc = last_e + 1
+                e_calc = e
             if e > last_e:
-                result += e - s + 1
-                last_e = e + 1
+                last_e = e
+
+            # print("s_calc = ", s_calc)
+            # print("e_calc = ", e_calc)
+            if s_calc <= e_calc:
+                inter = e_calc - s_calc + 1
+                result += inter
+            # print(" inter = ", inter)
+            print()
 
     print("\nresult = ", result)
 
